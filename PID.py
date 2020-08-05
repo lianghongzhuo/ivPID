f49cfd7417db09c3d24d2d96444d89d62ac894f2
Robert Jacobson <rljacobson@gmail.com>
2020-08-05 13:56:58 -0400

    Code cleanup and PEP normalization.

--- PID.py
+++ PID.py
@@ -20,9 +20,9 @@
 # description     :python pid controller
 # author          :Caner Durmusoglu
 # date            :20151218
-# version         :0.1
+# version         :0.2
 # notes           :
-# python_version  :2.7
+# python_version  :3.8
 # ==============================================================================
 
 """Ivmech PID Controller is simple implementation of a Proportional-Integral-Derivative (PID) Controller in the Python Programming Language.
@@ -34,35 +34,53 @@ class PID:
     """PID Controller
     """
 
-    def __init__(self, P=0.2, I=0.0, D=0.0, current_time=None):
+    # PID coefficients (gain)
+    Kp = 0.2
+    Ki = 0.0
+    Kd = 0.0
 
-        self.Kp = P
-        self.Ki = I
-        self.Kd = D
+    # Target value
+    target = 0.0
 
-        self.sample_time = 0.00
-        self.current_time = current_time if current_time is not None else time.time()
-        self.last_time = self.current_time
+    # Sample state
+    last_time = 0
+    current_time = 0
+    sample_time = 0.00
+    # Current values for the PID
+    PTerm = 0.0
+    ITerm = 0.0
+    DTerm = 0.0
+    # Difference between the target and the sample
+    last_error = 0.0
 
-        self.clear()
+    # Windup Guard
+    windup_guard = 20.0
 
-    def clear(self):
-        """Clears PID computations and coefficients"""
-        self.SetPoint = 0.0
+    output = 0.0
 
+    def __init__(self, p=0.2, i=0.0, d=0.0, sample_time=0.0, current_time=None):
+        self.Kp = p
+        self.Ki = i
+        self.Kd = d
+        self.sample_time = sample_time
+
+        # Initialize dynamically computed data members
+        self.clear(current_time)
+
+    def clear(self, current_time=None):
+        """Clears PID computations, coefficients, and target"""
+        self.target = 0.0
         self.PTerm = 0.0
         self.ITerm = 0.0
         self.DTerm = 0.0
         self.last_error = 0.0
-
-        # Windup Guard
-        self.int_error = 0.0
         self.windup_guard = 20.0
-
         self.output = 0.0
+        self.current_time = current_time if current_time is not None else time.time()
+        self.last_time = self.current_time
 
     def update(self, feedback_value, current_time=None):
-        """Calculates PID value for given reference feedback
+        """Calculates PID value for given reference feedback.
 
         .. math::
             u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}
@@ -73,44 +91,48 @@ class PID:
            Test PID with Kp=1.2, Ki=1, Kd=0.001 (test_pid.py)
 
         """
-        error = self.SetPoint - feedback_value
-
+        error = self.target - feedback_value
         self.current_time = current_time if current_time is not None else time.time()
         delta_time = self.current_time - self.last_time
-        delta_error = error - self.last_error
 
-        if (delta_time >= self.sample_time):
-            self.PTerm = self.Kp * error
+        if delta_time >= self.sample_time:
+
+            self.PTerm =  error
             self.ITerm += error * delta_time
 
-            if (self.ITerm < -self.windup_guard):
-                self.ITerm = -self.windup_guard
-            elif (self.ITerm > self.windup_guard):
-                self.ITerm = self.windup_guard
+            # Clamp ITerm between [-windup_guard, windup_guard]
+            self.ITerm = min(max(self.ITerm, -self.windup_guard), self.windup_guard)
 
             self.DTerm = 0.0
             if delta_time > 0:
+                delta_error = error - self.last_error
                 self.DTerm = delta_error / delta_time
 
             # Remember last time and last error for next calculation
             self.last_time = self.current_time
             self.last_error = error
 
-            self.output = self.PTerm + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
+            self.output = (self.Kp * self.PTerm) + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
 
-    def setKp(self, proportional_gain):
+        return self.output
+
+    def set_p_coefficient(self, proportional_gain):
         """Determines how aggressively the PID reacts to the current error with setting Proportional Gain"""
         self.Kp = proportional_gain
 
-    def setKi(self, integral_gain):
+    def set_i_coefficient(self, integral_gain):
         """Determines how aggressively the PID reacts to the current error with setting Integral Gain"""
         self.Ki = integral_gain
 
-    def setKd(self, derivative_gain):
+    def set_d_coefficient(self, derivative_gain):
         """Determines how aggressively the PID reacts to the current error with setting Derivative Gain"""
         self.Kd = derivative_gain
 
-    def setWindup(self, windup):
+    def set_target(self, new_target):
+        """The target value for the output of the PID"""
+        self.target = new_target
+
+    def set_windup(self, windup):
         """Integral windup, also known as integrator windup or reset windup,
         refers to the situation in a PID feedback controller where
         a large change in setpoint occurs (say a positive change)
@@ -122,8 +144,10 @@ class PID:
         """
         self.windup_guard = windup
 
-    def setSampleTime(self, sample_time):
+    def set_sample_time(self, sample_time):
         """PID that should be updated at a regular interval.
-        Based on a pre-determined sampe time, the PID decides if it should compute or return immediately.
+        Based on a pre-determined sample time, the PID decides if it should compute or return
+        immediately. Setting `sample_time` to zero effectively forces PID to recompute on every
+        call, which might be desirable if the client code handles the waiting itself.
         """
         self.sample_time = sample_time
--- test_pid.py
+++ test_pid.py
@@ -30,8 +30,8 @@ import PID
 import time
 import matplotlib.pyplot as plt
 import numpy as np
-#from scipy.interpolate import spline
-from scipy.interpolate import BSpline, make_interp_spline #  Switched to BSpline
+from scipy.interpolate import make_interp_spline
+
 
 def test_pid(P = 0.2,  I = 0.0, D= 0.0, L=100):
     """Self-test PID class
@@ -41,48 +41,44 @@ def test_pid(P = 0.2,  I = 0.0, D= 0.0, L=100):
         for i in range(1, END):
             pid.update(feedback)
             output = pid.output
-            if pid.SetPoint > 0:
+            if pid.target > 0:
                 feedback += (output - (1/i))
             if i>9:
-                pid.SetPoint = 1
+                pid.target = 1
             time.sleep(0.02)
         ---
     """
     pid = PID.PID(P, I, D)
 
-    pid.SetPoint=0.0
-    pid.setSampleTime(0.01)
+    pid.set_target(0.0)
+    pid.set_sample_time(0.01)
 
     END = L
     feedback = 0
 
     feedback_list = []
     time_list = []
-    setpoint_list = []
+    target_list = []
 
     for i in range(1, END):
-        pid.update(feedback)
-        output = pid.output
-        if pid.SetPoint > 0:
+        output = pid.update(feedback)
+        if pid.target > 0:
             feedback += (output - (1/i))
-        if i>9:
-            pid.SetPoint = 1
+        if i > 9:
+            pid.set_target(1)
         time.sleep(0.02)
 
         feedback_list.append(feedback)
-        setpoint_list.append(pid.SetPoint)
+        target_list.append(pid.target)
         time_list.append(i)
 
     time_sm = np.array(time_list)
     time_smooth = np.linspace(time_sm.min(), time_sm.max(), 300)
-
-    # feedback_smooth = spline(time_list, feedback_list, time_smooth)
-    # Using make_interp_spline to create BSpline
     helper_x3 = make_interp_spline(time_list, feedback_list)
     feedback_smooth = helper_x3(time_smooth)
 
     plt.plot(time_smooth, feedback_smooth)
-    plt.plot(time_list, setpoint_list)
+    plt.plot(time_list, target_list)
     plt.xlim((0, L))
     plt.ylim((min(feedback_list)-0.5, max(feedback_list)+0.5))
     plt.xlabel('time (s)')
@@ -93,6 +89,7 @@ def test_pid(P = 0.2,  I = 0.0, D= 0.0, L=100):
 
     plt.grid(True)
     plt.show()
+
 
 if __name__ == "__main__":
     test_pid(1.2, 1, 0.001, L=50)
